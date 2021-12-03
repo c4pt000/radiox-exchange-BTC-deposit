a0e1aed093cc4c86ebff75f5302fafb73fc9c1f7
Your Name <you@example.com>
2021-12-03 07:15:04 -0500

    removed all fees (mostly down to 0.00006~ BTC on a maker / taker at 0.01 BTC)

--- desktop/package/macosx/copy_dbs.sh
+++ desktop/package/macosx/copy_dbs.sh
@@ -4,13 +4,13 @@ cd $(dirname $0)/../../../
 
 version="1.7.5"
 
-# Set BISQ_DIR as environment var to the path of your locally synced Bisq data directory e.g. BISQ_DIR=~/Library/Application\ Support/Bisq
+# Set RADIOX_DIR as environment var to the path of your locally synced RadioX data directory e.g. RADIOX_DIR=~/Library/Application\ Support/RadioX
 
-dbDir=$BISQ_DIR/btc_mainnet/db
+dbDir=$RADIOX_DIR/btc_mainnet/db
 resDir=p2p/src/main/resources
 
 # Only commit new TradeStatistics3Store if you plan to add it to
-# https://github.com/bisq-network/bisq/blob/0345c795e2c227d827a1f239a323dda1250f4e69/common/src/main/java/bisq/common/app/Version.java#L40 as well.
+# https://github.com/bisq-network/radiox/blob/0345c795e2c227d827a1f239a323dda1250f4e69/common/src/main/java/radiox/common/app/Version.java#L40 as well.
 cp "$dbDir/TradeStatistics3Store" "$resDir/TradeStatistics3Store_${version}_BTC_MAINNET"
 cp "$dbDir/AccountAgeWitnessStore" "$resDir/AccountAgeWitnessStore_${version}_BTC_MAINNET"
 cp "$dbDir/DaoStateStore" "$resDir/DaoStateStore_BTC_MAINNET"
--- desktop/package/macosx/finalize.sh
+++ desktop/package/macosx/finalize.sh
@@ -6,10 +6,10 @@ version="1.7.5-SNAPSHOT"
 
 target_dir="releases/$version"
 
-# Set BISQ_GPG_USER as environment var to the email address used for gpg signing. e.g. BISQ_GPG_USER=manfred@bitsquare.io
-# Set BISQ_VM_PATH as environment var to the directory where your shared folders for virtual box are residing
+# Set RADIOX_GPG_USER as environment var to the email address used for gpg signing. e.g. RADIOX_GPG_USER=manfred@bitsquare.io
+# Set RADIOX_VM_PATH as environment var to the directory where your shared folders for virtual box are residing
 
-vmPath=$BISQ_VM_PATH
+vmPath=$RADIOX_VM_PATH
 linux64=$vmPath/vm_shared_ubuntu
 win64=$vmPath/vm_shared_windows
 macos=$vmPath/vm_shared_macosx
@@ -29,19 +29,19 @@ cp "$target_dir/../../package/29CDFD3B.asc" "$target_dir/"
 # signing key
 cp "$target_dir/../../package/signingkey.asc" "$target_dir/"
 
-dmg="Bisq-$version.dmg"
+dmg="RadioX-$version.dmg"
 cp "$macos/$dmg" "$target_dir/"
 
-deb="bisq_$version-1_amd64.deb"
-deb64="Bisq-64bit-$version.deb"
+deb="radiox_$version-1_amd64.deb"
+deb64="RadioX-64bit-$version.deb"
 cp "$linux64/$deb" "$target_dir/$deb64"
 
-rpm="bisq-$version-1.x86_64.rpm"
-rpm64="Bisq-64bit-$version.rpm"
+rpm="radiox-$version-1.x86_64.rpm"
+rpm64="RadioX-64bit-$version.rpm"
 cp "$linux64/$rpm" "$target_dir/$rpm64"
 
-exe="Bisq-$version.exe"
-exe64="Bisq-64bit-$version.exe"
+exe="RadioX-$version.exe"
+exe64="RadioX-64bit-$version.exe"
 cp "$win64/$exe" "$target_dir/$exe64"
 
 rpi="jar-lib-for-raspberry-pi-$version.zip"
@@ -50,20 +50,20 @@ cp "$macos/$rpi" "$target_dir/"
 # create file with jar signatures
 cat "$macos/desktop-$version-all-mac.jar.SHA-256" \
 "$linux64/desktop-$version-all-linux.jar.SHA-256" \
-"$win64/desktop-$version-all-win.jar.SHA-256" > "$target_dir/Bisq-$version.jar.txt"
+"$win64/desktop-$version-all-win.jar.SHA-256" > "$target_dir/RadioX-$version.jar.txt"
 
-sed -i '' '1 s_^_macOS: _' "$target_dir/Bisq-$version.jar.txt"
-sed -i '' '2 s_^_linux: _' "$target_dir/Bisq-$version.jar.txt"
-sed -i '' '3 s_^_windows: _' "$target_dir/Bisq-$version.jar.txt"
+sed -i '' '1 s_^_macOS: _' "$target_dir/RadioX-$version.jar.txt"
+sed -i '' '2 s_^_linux: _' "$target_dir/RadioX-$version.jar.txt"
+sed -i '' '3 s_^_windows: _' "$target_dir/RadioX-$version.jar.txt"
 
 cd "$target_dir"
 
 echo Create signatures
-gpg --digest-algo SHA256 --local-user $BISQ_GPG_USER --output $dmg.asc --detach-sig --armor $dmg
-gpg --digest-algo SHA256 --local-user $BISQ_GPG_USER --output $deb64.asc --detach-sig --armor $deb64
-gpg --digest-algo SHA256 --local-user $BISQ_GPG_USER --output $rpm64.asc --detach-sig --armor $rpm64
-gpg --digest-algo SHA256 --local-user $BISQ_GPG_USER --output $exe64.asc --detach-sig --armor $exe64
-gpg --digest-algo SHA256 --local-user $BISQ_GPG_USER --output $rpi.asc --detach-sig --armor $rpi
+gpg --digest-algo SHA256 --local-user $RADIOX_GPG_USER --output $dmg.asc --detach-sig --armor $dmg
+gpg --digest-algo SHA256 --local-user $RADIOX_GPG_USER --output $deb64.asc --detach-sig --armor $deb64
+gpg --digest-algo SHA256 --local-user $RADIOX_GPG_USER --output $rpm64.asc --detach-sig --armor $rpm64
+gpg --digest-algo SHA256 --local-user $RADIOX_GPG_USER --output $exe64.asc --detach-sig --armor $exe64
+gpg --digest-algo SHA256 --local-user $RADIOX_GPG_USER --output $rpi.asc --detach-sig --armor $rpi
 
 echo Verify signatures
 gpg --digest-algo SHA256 --verify $dmg{.asc*,}
